9cd4c7c3a354d3d1c4f804b4d7d08f94afd79e9b
siarasmall <siarasmall19@gmail.com>
2018-07-18 14:41:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
   end
+  
+  post '/food' do
+    "My name is #{params[:user_name]} and I love #{params[:fav_food]}"
+  
+  end 
 
   # Add your post route and action below
 
